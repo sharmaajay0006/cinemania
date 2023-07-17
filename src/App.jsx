66bbc0d9e485caf6cc6532525064ceb05569068d
Ajay Sharma <sharmaajaymgs297@gmail.com>
2023-07-18 05:08:39 +0530

    component creation done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 import { fetchDataFromApi } from "./utils/Api"
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home)
   useEffect(() => apiTesting(), []);
 
 
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((response) => {
       console.log(response);
+      dispatch(getApiConfiguration(response));
     })
   };
 
-  return <div className='App'>App</div>
+  return <div className='App'>App
+    {url?.total_pages}
+  </div>
 
 }
 
